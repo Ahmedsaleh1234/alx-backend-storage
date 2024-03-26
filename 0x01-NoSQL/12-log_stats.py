7b18d452e21c4cf74461e774746b5f9ad815bd9f
Ahmedsaleh1234 <a12661826@gmail.com>
2024-03-27 00:19:45 +0200

    12-log_stats.py

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -1,22 +1,25 @@
 #!/usr/bin/env python3
-""" Log stats"""
+""" 12. Log stats
+"""
+
 
 from pymongo import MongoClient
 
 
 def log_stats():
-    """main"""
+    """ log_stats.
+    """
     client = MongoClient('mongodb://127.0.0.1:27017')
-    mongo_collection = client.logs.nginx
-    logs = mongo_collection.count_documents({})
-    get = mongo_collection.count_documents({"method": "GET"})
-    post = mongo_collection.count_documents({"method": "POST"})
-    put = mongo_collection.count_documents({"method": "PUT"})
-    patch = mongo_collection.count_documents({"method": "PATCH"})
-    delete = mongo_collection.count_documents({"method": "DELETE"})
-    path = mongo_collection.count_documents(
+    logs_collection = client.logs.nginx
+    total = logs_collection.count_documents({})
+    get = logs_collection.count_documents({"method": "GET"})
+    post = logs_collection.count_documents({"method": "POST"})
+    put = logs_collection.count_documents({"method": "PUT"})
+    patch = logs_collection.count_documents({"method": "PATCH"})
+    delete = logs_collection.count_documents({"method": "DELETE"})
+    path = logs_collection.count_documents(
         {"method": "GET", "path": "/status"})
-    print(f"{logs} logs")
+    print(f"{total} logs")
     print("Methods:")
     print(f"\tmethod GET: {get}")
     print(f"\tmethod POST: {post}")
@@ -26,5 +29,5 @@ def log_stats():
     print(f"{path} status check")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     log_stats()
---
+++ 0x01-NoSQL/test2.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+""" Log stats"""
+
+from pymongo import MongoClient
+
+
+def log_stats():
+    """main"""
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    mongo_colection = client.logs.nginx
+    logs = mongo_collection.count_documents({})
+    get = mongo_collection.count_documents({"method": "GET"})
+    post = mongo_collection.count_documents({"method": "POST"})
+    put = mongo_collection.count_documents({"method": "PUT"})
+    patch = mongo_collection.count_documents({"method": "PATCH"})
+    delete = mongo_collection.count_documents({"method": "DELETE"})
+    path = mongo_collection.count_documents(
+        {"method": "GET", "path": "/status"})
+    print(f"{logs} logs")
+    print("Methods:")
+    print(f"\tmethod GET: {get}")
+    print(f"\tmethod POST: {post}")
+    print(f"\tmethod PUT: {put}")
+    print(f"\tmethod PATCH: {patch}")
+    print(f"\tmethod DELETE: {delete}")
+    print(f"{path} status check")
+
+
+if __name__ == '__main__':
+    log_stats()
