080c29a317fa5cbc7fc88d0c2f89ea39e187091c
Ahmedsaleh1234 <a12661826@gmail.com>
2024-03-28 00:18:56 +0200

    exercise.py

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """Writing strings to Redis"""
 import redis
-from typing import Union
+from typing import Union, Optional, Callable
 from uuid import uuid4
 
 
@@ -25,3 +25,28 @@ class Cache:
         key = str(uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Optional[Callable]):
+        """
+        method that take a key string argument and
+        an optional Callable argument named fn
+        """
+        client = self._redis
+        value = client.get(key)
+        if not value:
+            return
+        if fn is int:
+            return self.get_int(value)
+        if fn is str:
+            return self.get_str(value)
+        if callable(fn):
+            return fn(value)
+        return value
+
+    def get_str(self, date: bytes) -> str:
+        """"convert bytes to string"""
+        return date.decode('utf-8')
+
+    def get_int(self, data: bytes) -> int:
+        """convert bytes to intger"""
+        return int(data)
